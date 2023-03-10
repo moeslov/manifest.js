7d483169c34cabf8f92b4bd8a7ba8d6b1541e328
moeslov <moeslov@gmail.com>
2023-03-10 16:26:31 +0700

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,75 @@
+{
+
+  "name": "The Coffee Store",
+
+  "short_name": "TheCoffeeStore",
+
+  "start_url": "https://overlandcamp.id/coffee/",
+
+  "display": "Deliver Coffee to Your Door",
+
+  "background_color": "#fff",
+
+  "theme_color": "#243b99",
+
+  "description": "Toko Online Kopi Terlengkap",
+
+  "icons": [{
+
+    "src": "https://overlandcamp.id/coffee/48.png",
+
+    "sizes": "48x48",
+
+    "type": "image/png"
+
+  }, {
+
+    "src": "https://overlandcamp.id/coffee/72.png",
+
+    "sizes": "72x72",
+
+    "type": "image/png"
+
+  }, {
+
+    "src": "https://overlandcamp.id/coffee/96.png",
+
+    "sizes": "96x96",
+
+    "type": "image/png"
+
+  }, {
+
+    "src": "https://overlandcamp.id/coffee/144.png",
+
+    "sizes": "144x144",
+
+    "type": "image/png"
+
+  }, {
+
+    "src": "https://overlandcamp.id/coffee/168.png",
+
+    "sizes": "168x168",
+
+    "type": "image/png"
+
+  }, {
+
+    "src": "https://overlandcamp.id/coffee/192.png",
+
+    "sizes": "192x192",
+
+    "type": "image/png"
+
+  }, {
+
+    "src": "https://overlandcamp.id/coffee/512.png",
+
+    "sizes": "512x512",
+
+    "type": "image/png"
+
+  }]
+
+}
